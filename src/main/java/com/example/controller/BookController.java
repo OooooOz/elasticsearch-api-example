d9eb0c94194d9e984fe86f61e6a52409150ad51a
OooooOz <zhongwenhao@gerpgo.com>
2022-01-19 16:11:56 +0800

    elasticsearch-get

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.dao.BookRepository;
 import com.example.model.Book;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +11,9 @@ import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -37,7 +40,7 @@ public class BookController {
      * Book实体类已经创建有映射，对应索引books，调用save方法会保持到对应索引下
      * 已有数据会更新
      *
-     * @param book { "id":"2" ,"name":"hello","price":80 }
+     * @param book { "id":"1" ,"name":"hello","price":80 }
      * @return
      */
     @PostMapping("/book/save")
@@ -61,7 +64,7 @@ public class BookController {
         return elasticsearchOperations.index(indexQuery, IndexCoordinates.of("books"));
     }
 
-//    调用报错
+//    // 调用报错
 //    @PostMapping("/book/update")
 //    public String update(@RequestBody Book book) {
 //        Criteria criteria = new Criteria("id").is(book.getId());
@@ -91,49 +94,125 @@ public class BookController {
         return elasticsearchOperations.delete(book, IndexCoordinates.of("books"));
     }
 
+    /**
+     * 通过ElasticsearchRepository来查询所有数据
+     * @return
+     */
+    @GetMapping("/book/findAll")
+    public List<Book> findAll() {
+        List<Book> list = new ArrayList<>();
+        Iterable<Book> books = bookRepository.findAll();
+        books.forEach(book -> list.add(book));
+        return list;
+    }
+
+    /**
+     * 通过elasticsearchOperations来查询所有数据
+     * NativeSearchQueryBuilder：Spring提供的一个查询条件构建器，帮助构建json格式的请求体
+     * @return
+     */
+    @GetMapping("/book/searchAll")
+    public List<Book> searchAll() {
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        SearchHits<Book> search = elasticsearchOperations.search(queryBuilder.build(),Book.class);
+        List<Book> list = search.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
+        return list;
+    }
+
+    /**
+     * 根据唯_id获取数据
+     * @param id
+     * @return
+     */
+    @GetMapping("/book/{id}")
+    public Book get(@PathVariable("id") String id) {
+        // 通过bookRepository来获取指定条件数据
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        System.out.println("findById: " + optionalBook.get().toString());
+
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // 添加基本的分词查询
+        queryBuilder.withQuery(QueryBuilders.matchQuery("id", id));
+        SearchHit<Book> search = elasticsearchOperations.searchOne(queryBuilder.build(),Book.class);
+        System.out.println("searchOne: " + search.getContent().toString());
+
+        Book book = elasticsearchOperations.get(id, Book.class);
+//        还可以指定其它索引库
+//        Book book = elasticsearchOperations.get(id, Book.class, IndexCoordinates.of("books"));
+        return book;
+    }
+
+    /**
+     * 单条件匹配查询
+     * {id='2', name='匹配查询name81', price=81}
+     * {id='3', name='匹配查询name82', price=82}
+     * {id='4', name='匹配81', price=80}
+     * @return
+     */
+    @GetMapping("/book/query/name")
+    public List<Book> getByName() {
+        String name = "匹配查询name";
+        String termName = "匹配查询name82";
+
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // match类型查询并不是全匹配,比如此处会把三条记录都查出来
+        queryBuilder.withQuery(QueryBuilders.matchQuery("name", name));
+
+        SearchHits<Book> searchs = elasticsearchOperations.search(queryBuilder.build(),Book.class);
+        List<Book> list = searchs.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
+        System.out.println("matchQuerySearch: " + list);
+
+        // 默认取第一个
+        SearchHit<Book> search = elasticsearchOperations.searchOne(queryBuilder.build(),Book.class);
+        System.out.println("matchQuerySearchOne: " + search.getContent().toString());
+
+        // term类型查询是精确查询
+        NativeSearchQueryBuilder termQueryBuilder = new NativeSearchQueryBuilder();
+        termQueryBuilder.withQuery(QueryBuilders.termQuery("name", termName));
+        searchs = elasticsearchOperations.search(termQueryBuilder.build(),Book.class);
+        list = searchs.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
+        System.out.println("termQuerySearch: " + list);
+
+        // CriteriaQuery精确词条匹配
+        Criteria criteria = new Criteria("name").is(termName);
+        Query criteriaQuery = new CriteriaQuery(criteria);
+        searchs = elasticsearchOperations.search(criteriaQuery, Book.class);
+        list = searchs.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
+        System.out.println("CriteriaQuerySearch: " + list);
+        return list;
+    }
+
+    /**
+     * 多字段查询：从多个字段中检索含有关键字的数据
+     * @return
+     */
+    @GetMapping("/book/query")
+    public List<Book> getByNameAndPrice() {
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        // multiMatchQuery多字段查询，查询name、price任一字段中含有81的数据
+        queryBuilder.withQuery(QueryBuilders.multiMatchQuery("81", "name","price"));
+        SearchHits<Book> searches = elasticsearchOperations.search(queryBuilder.build(),Book.class);
+        List<Book> list = searches.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
+        System.out.println("multiMatchQuerySearch: " + list);
+
+        return list;
+    }
 
     // --------------------------------------------------------------------------------------- //
 
 
     /**
-     * curl -X GET http://localhost:8080/book/1
+     * 通过ElasticsearchRepository自定义方法实现多查询条件查询
      *
-     * @param id
      * @return
      */
-    @GetMapping("/book/{id}")
-    public Book findById(@PathVariable("id") String id) {
-        Book book = elasticsearchOperations.get(id, Book.class, IndexCoordinates.of("books"));
-        return book;
-    }
-
-    /**
-     * curl -X GET 'http://localhost:8080/query/hello'
-     *
-     * @param name
-     * @return
-     */
-    @GetMapping("/query/{name}")
-    public List<Book> query(@PathVariable("name") String name) {
-        List<Book> result = bookRepository.findByNameAndPrice(name, 80);
+    @GetMapping("/book/query/name/price")
+    public List<Book> query() {
+        List<Book> result = bookRepository.findByNameAndPrice("匹配查询name82", 82);
         return result;
     }
 
-    /**
-     * curl -X GET 'http://localhost:8080/search/hello'
-     *
-     * @param name
-     * @return
-     */
-    @GetMapping("/search/{name}")
-    public List<Book> search(@PathVariable("name") String name) {
-        // Criteria criteria = new Criteria("name").is("hello").and("price").greaterThan(30);
-        Criteria criteria = new Criteria("name").is(name);
-        Query criteriaQuery = new CriteriaQuery(criteria);
-        SearchHits<Book> search = elasticsearchOperations.search(criteriaQuery, Book.class);
-        List<Book> list = search.getSearchHits().stream().map(item -> item.getContent()).collect(Collectors.toList());
-        return list;
-    }
 
     /**
      * curl http://localhost:8080/page/hello
--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -46,4 +46,13 @@ public class Book {
     public void setPrice(Integer price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
